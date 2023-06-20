eb90c8ea66b72c11205999164e1c6edc36c835a4
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-06-20 01:20:28 -0700

    add: DashboardSearch component

---
+++ WelbyFrontend/welbyfrontend/src/components/DashboardSearch/index.tsx
@@ -0,0 +1,22 @@
+import { Icon, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { BsSearch } from 'react-icons/bs';
+
+const DashboardSearch = () => {
+  return (
+    <InputGroup>
+      <InputLeftElement>
+        <Icon as={BsSearch} color="#24a2f0" />
+      </InputLeftElement>
+      <Input
+        bg="ffffff"
+        cursor="text"
+        border="2px solid #f6f6f6"
+        fontFamily="Montserrat"
+        fontWeight="400"
+        placeholder="Search"
+      />
+    </InputGroup>
+  );
+};
+
+export default DashboardSearch;

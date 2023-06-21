3bb7334fbf9348250490a1d5bf0a6fdc00bc1ee5
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-06-21 20:03:16 +0800

    add: installed react-router-dom

--- WelbyFrontend/welbyfrontend/src/App.tsx
+++ WelbyFrontend/welbyfrontend/src/App.tsx
@@ -1,11 +1,21 @@
-import AdminDashboard from './pages/AdminDashboard';
+import Dashboard from './pages/AdminDashboard';
+//import SignUpPage from './pages/SignUp';
 import LoginPage from './pages/Login';
+//import WelcomePage from './pages/Welcome'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
     return (
-        <LoginPage />
-  );
+        <BrowserRouter>
+            <Routes>
+                {/*<Route path="/" element={<WelcomePage />} />*/}
+                <Route path="employee/login" element={<LoginPage />} />
+                {/*<Route path="employee/signup" element={<SignUpPage />} />*/}
+                <Route path="employee/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;

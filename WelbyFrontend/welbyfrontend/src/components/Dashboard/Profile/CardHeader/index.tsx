f5dddc043ccd6c706bbe3a6db692d22e929e3199
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-06-22 15:30:38 +0800

    add: Profile CardHeader component

---
+++ WelbyFrontend/welbyfrontend/src/components/Dashboard/Profile/CardHeader/index.tsx
@@ -0,0 +1,13 @@
+import { CardHeader } from "@chakra-ui/react";
+
+type ProfileCardHeaderProps = {
+    children: string;
+};
+
+const ProfileCardHeader = ({ children }: ProfileCardHeaderProps) => {
+    return (
+        <CardHeader fontFamily="Montserrat" fontWeight="600" fontSize="2xl">{children}</CardHeader>
+    );
+};
+
+export default ProfileCardHeader;

598f36391e610971b7482a73268d3ae267ee27f4
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-06-24 08:35:35 +0800

    feat: add "w" property, change "my" value to 16, and surround Card with Center

--- WelbyFrontend/welbyfrontend/src/components/Main/FormCard/index.tsx
+++ WelbyFrontend/welbyfrontend/src/components/Main/FormCard/index.tsx
@@ -1,22 +1,26 @@
-import { Card } from '@chakra-ui/react';
+import { Card, Center } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
 type MainFormCardProps = {
-  children: ReactNode;
+    children: ReactNode;
+    w?: string;
 };
 
-const MainFormCard = ({ children }: MainFormCardProps) => {
-  return (
-    <Card
-      boxShadow="dark-lg"
-      bg="#24a2f0"
-      p={{ base: '8', md: '16' }}
-      my="5rem"
-      borderRadius="2xl"
-    >
-      {children}
-    </Card>
-  );
+const MainFormCard = ({ children, w }: MainFormCardProps) => {
+    return (
+        <Center>
+            <Card
+                boxShadow="dark-lg"
+                bg="#24a2f0"
+                my="16"
+                p={{ base: '8', md: '16' }}
+                borderRadius="2xl"
+                w={w}
+            >
+                {children}
+            </Card>
+        </Center>
+    );
 };
 
 export default MainFormCard;

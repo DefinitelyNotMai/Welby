b4b2f8b96e8760dd60a1bdfb49a3dfb49512cb2d
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-06-24 08:35:57 +0800

    feat: Render pages for Company and Employee

--- WelbyFrontend/welbyfrontend/src/App.tsx
+++ WelbyFrontend/welbyfrontend/src/App.tsx
@@ -1,20 +1,31 @@
-import Dashboard from './pages/Dashboard';
-import ProfilePage from './pages/Dashboard/Profile';
-//import SignUpPage from './pages/SignUp';
-import LoginPage from './pages/Login';
-//import WelcomePage from './pages/Welcome'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import WelcomePage from './pages/Welcome'
+/* Admin */
+/* Company */
+import CompanySignUpPage from './pages/Company/SignUp';
+import CompanyLoginPage from './pages/Company/Login';
+import CompanyDashboard from './pages/Company/Dashboard';
+import CompanyProfilePage from './pages/Company/Dashboard/Profile';
+/* Employee */
+import EmployeeSignUpPage from './pages/Employee/SignUp';
+import EmployeeLoginPage from './pages/Employee/Login';
+import EmployeeDashboard from './pages/Employee/Dashboard';
+import EmployeeProfilePage from './pages/Employee/Dashboard/Profile';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/*<Route path="/" element={<WelcomePage />} />*/}
-                <Route path="employee/login" element={<LoginPage />} />
-                {/*<Route path="employee/signup" element={<SignUpPage />} />*/}
-                <Route path="employee/dashboard" element={<Dashboard />} />
-                <Route path="employee/dashboard/profile" element={<ProfilePage />} />
+                <Route path="/" element={<WelcomePage />} />
+                <Route path="employee/login" element={<EmployeeLoginPage />} />
+                <Route path="employee/signup" element={<EmployeeSignUpPage />} />
+                <Route path="employee/dashboard" element={<EmployeeDashboard />} />
+                <Route path="employee/dashboard/profile" element={<EmployeeProfilePage />} />
+                <Route path="company/login" element={<CompanyLoginPage />} />
+                <Route path="company/signup" element={<CompanySignUpPage />} />
+                <Route path="company/dashboard" element={<CompanyDashboard />} />
+                <Route path="company/dashboard/profile" element={<CompanyProfilePage />} />
             </Routes>
         </BrowserRouter>
     );

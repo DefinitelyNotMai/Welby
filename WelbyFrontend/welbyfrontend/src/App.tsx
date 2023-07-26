c0d055bb4db80d53aa19a4488e2e28a8b93a0ad7
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-07-26 14:32:17 +0800

    feat: Removed Login, Signup, and Dashboard pages for Company and Employee. Use only a single page / route for these.

--- WelbyFrontend/welbyfrontend/src/App.tsx
+++ WelbyFrontend/welbyfrontend/src/App.tsx
@@ -1,16 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { UserContextProvider } from './context/UserContext';
 import WelcomePage from './pages/Welcome'
-/* Admin */
-/* Company */
-import CompanySignUpPage from './pages/Company/SignUp';
-import CompanyLoginPage from './pages/Company/Login';
-import CompanyDashboard from './pages/Company/Dashboard';
-/* Employee */
-import EmployeeSignUpPage from './pages/Employee/SignUp';
-import EmployeeLoginPage from './pages/Employee/Login';
-import EmployeeDashboard from './pages/Employee/Dashboard';
-import EmployeeProfilePage from './pages/Employee/Dashboard/Profile';
-import {UserContextProvider} from './context/UserContext';
+import LoginPage from './pages/Login'
+import SignUpPage from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
 function App() {
     return (
@@ -18,13 +11,9 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<WelcomePage />} />
-                    <Route path="employee/login" element={<EmployeeLoginPage />} />
-                    <Route path="employee/signup" element={<EmployeeSignUpPage />} />
-                    <Route path="employee/dashboard" element={<EmployeeDashboard />} />
-                    <Route path="employee/dashboard/profile" element={<EmployeeProfilePage />} />
-                    <Route path="company/login" element={<CompanyLoginPage />} />
-                    <Route path="company/signup" element={<CompanySignUpPage />} />
-                    <Route path="company/dashboard" element={<CompanyDashboard />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
                 </Routes>
             </BrowserRouter>
         </UserContextProvider>

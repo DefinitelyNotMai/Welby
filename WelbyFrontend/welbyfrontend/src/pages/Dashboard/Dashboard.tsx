8a6e8dc1d41dde5134084b2c484d28ce886161bf
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-08-25 11:53:01 +0800

    feat: Render respective Tab components for clicked DashboardSidebarItem.

--- WelbyFrontend/welbyfrontend/src/pages/Dashboard/Dashboard.tsx
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,33 @@
 import { Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useUserContext } from '../../context/UserContext';
+import { LuLayoutDashboard } from 'react-icons/lu';
+import { AiOutlineTeam } from 'react-icons/ai';
+import { MdOutlineBusinessCenter } from 'react-icons/md';
+import axios from 'axios';
 import DashboardHeader from '../../components/Dashboard/Header';
 import DashboardSidebar from '../../components/Dashboard/Sidebar';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useUserContext } from '../../context/UserContext';
+import DashboardSidebarItem from '../../components/Dashboard/SidebarItem';
+import MyDashboard from './MyDashboard';
+import MyTeam from './MyTeam';
+import OurCompany from './OurCompany/OurCompany';
+
+type ComponentName = 'My Dashboard' | 'My Team' | 'Our Company';
 
 const Dashboard = () => {
+    const [activeItem, setActiveItem] = useState<ComponentName>('My Dashboard');
     const { userId } = useUserContext();
     const [Nickname, setNickname] = useState('');
+
+    const handleItemClick = (itemName: ComponentName) => {
+        setActiveItem(itemName);
+    };
+
+    const components: Record<ComponentName, JSX.Element> = {
+        'My Dashboard': <MyDashboard />,
+        'My Team': <MyTeam />,
+        'Our Company': <OurCompany />,
+    };
 
     useEffect(() => {
         console.log(userId)
@@ -36,15 +55,38 @@ const Dashboard = () => {
         fetchUserData();
     }, []);
 
-    const userName = "Hello"
-    //console.log(state.id);
 
     return (
         <Flex flexDirection="column">
             <DashboardHeader name={Nickname} />
             <Flex flexDirection="row">
-                <DashboardSidebar />
-                {/* TODO: navigation for each SidebarItem*/}
+                <DashboardSidebar>
+                    <DashboardSidebarItem
+                        icon={LuLayoutDashboard}
+                        color={activeItem === 'My Dashboard' ? '#89b4fa' : '#bcbcbc'}
+                        onClick={() => handleItemClick('My Dashboard')}
+                    >
+                        My Dashboard
+                    </DashboardSidebarItem>
+                    <DashboardSidebarItem
+                        icon={AiOutlineTeam}
+                        color={activeItem === 'My Team' ? '#89b4fa' : '#bcbcbc'}
+                        onClick={() => handleItemClick('My Team')}
+                    >
+                        My Team
+                    </DashboardSidebarItem>
+                    <DashboardSidebarItem
+                        icon={MdOutlineBusinessCenter}
+                        color={activeItem === 'Our Company' ? '#89b4fa' : '#bcbcbc'}
+                        onClick={() => handleItemClick('Our Company')}
+                    >
+                        Our Company
+                    </DashboardSidebarItem>
+                </DashboardSidebar>
+
+                <Flex flexDirection="column" bg="#f2f2f2" height="100vh" width="100vw">
+                    {components[activeItem]}
+                </Flex>
             </Flex>
         </Flex>
     );

61e013b788f310d8fe3ac80af1bb57179f6a6a46
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-08-25 11:59:24 +0800

    add: MyDashboard Feedback content.

---
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyDashboard/Feedbacks.tsx
@@ -0,0 +1,11 @@
+import Card from "../../../components/Dashboard/Card";
+
+const Feedbacks = () => {
+    return (
+        <>
+            <Card title="Feedbacks">Placeholder Text</Card>
+        </>
+    );
+};
+
+export default Feedbacks;

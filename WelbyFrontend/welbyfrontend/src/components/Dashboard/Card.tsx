e6c6d732873dde3a05e2ea972710bbd36c41c792
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-20 05:26:30 +0800

    refactor: Prop changes and additions for Card component.

--- WelbyFrontend/welbyfrontend/src/components/Dashboard/Card.tsx
+++ WelbyFrontend/welbyfrontend/src/components/Dashboard/Card.tsx
@@ -1,28 +1,56 @@
-import { Card, CardBody, CardHeader, Text } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import { Card, CardBody, Flex, Grid, Icon, Text } from '@chakra-ui/react';
 
 type DashboardCardProps = {
-    children: ReactNode;
-    title?: string;
+    icon?: React.ElementType;
     mb?: string;
+    dataValue?: number;
+    title?: string;
 };
 
-const DashboardCard = ({ children, title, mb }: DashboardCardProps) => {
+const DashboardCard = ({
+    icon: IconComponent,
+    mb,
+    dataValue,
+    title,
+}: DashboardCardProps) => {
     return (
-        <Card
-            boxShadow="md"
-            bg="#ffffff"
-            p="0"
-            borderRadius="1rem 0 0 1rem"
-            mb={mb}
-        >
-            <CardHeader borderBottom="2px solid #ebebeb">
-                <Text fontFamily="Montserrat" fontWeight="700" fontSize="18">
-                    {title}
-                </Text>
-            </CardHeader>
-            <CardBody>{children}</CardBody>
-        </Card>
+        <>
+            <Card boxShadow="md" bg="#ffffff" p="0" borderRadius="1rem" mb={mb}>
+                <CardBody>
+                    <Grid templateColumns="1fr 2fr 1fr" gap="4">
+                        <Flex alignItems="center">
+                            <Icon as={IconComponent} boxSize="16" color="#24a2f0" />
+                        </Flex>
+                        <Flex flexDirection="column">
+                            <Text
+                                color="#7c7c7c"
+                                fontFamily="Montserrat"
+                                fontWeight="700"
+                                fontSize="sm"
+                            >
+                                {title}
+                            </Text>
+                            <Text
+                                color="#2f2f2f"
+                                fontFamily="Montserrat"
+                                fontWeight="800"
+                                fontSize="32"
+                            >
+                                {dataValue}%
+                            </Text>
+                            <Text
+                                color="#727272"
+                                fontFamily="Montserrat"
+                                fontWeight="600"
+                                fontSize="sm"
+                            >
+                                Previous Week
+                            </Text>
+                        </Flex>
+                    </Grid>
+                </CardBody>
+            </Card>
+        </>
     );
 };
 

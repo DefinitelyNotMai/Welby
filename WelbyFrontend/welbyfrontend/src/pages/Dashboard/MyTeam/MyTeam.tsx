dfa00d74403ccee9617e303d678fd9731990c59a
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-22 22:06:49 +0800

    feat: Added ContentMapping for MyTeam page.

--- WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/MyTeam.tsx
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/MyTeam.tsx
@@ -1,14 +1,44 @@
 import { Flex } from '@chakra-ui/react';
+import { useState } from 'react';
 import DashboardTab from '../../../components/Dashboard/Tab';
 import DashboardTabItem from '../../../components/Dashboard/TabItem';
+import Overview from './Overview';
+
+type ContentMapping = {
+    [key: string]: React.ReactNode;
+};
 
 const MyTeam = () => {
+    const [selectedItem, setSelectedItem] = useState<string | null>('Overview');
+
+    const handleItemClick = (itemName: string) => {
+        setSelectedItem(itemName);
+    };
+
+    const contentMapping: ContentMapping = {
+        Overview: <Overview />,
+        //TeamProfile: <TeamProfile />,
+    };
+
     return (
-        <Flex flexDirection="row">
+        <Flex flexDirection="column">
             <DashboardTab>
-                <DashboardTabItem>Overview</DashboardTabItem>
-                <DashboardTabItem>Team Profile</DashboardTabItem>
+                <DashboardTabItem
+                    color={selectedItem === 'Overview' ? '#89b4fa' : '#bcbcbc'}
+                    onClick={() => handleItemClick('Overview')}
+                >
+                    Overview
+                </DashboardTabItem>
+                <DashboardTabItem
+                    color={selectedItem === 'TeamProfile' ? '#89b4fa' : '#bcbcbc'}
+                    onClick={() => handleItemClick('TeamProfile')}
+                >
+                    Team Profile
+                </DashboardTabItem>
             </DashboardTab>
+            <Flex flexDirection="column" mt="4" ml="4">
+                {selectedItem && contentMapping[selectedItem]}
+            </Flex>
         </Flex>
     );
 };

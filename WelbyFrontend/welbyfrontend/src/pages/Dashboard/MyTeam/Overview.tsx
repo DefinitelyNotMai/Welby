72a3a3196df80c3c248067b828cad46d60d6b2ad
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-22 22:05:07 +0800

    add: MyTeam/Overview page.

---
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/Overview.tsx
@@ -0,0 +1,12 @@
+import Section from "../../../components/Dashboard/Section";
+
+const Overview = () => {
+    return (
+        <>
+            <Section title="Team">
+            </Section>
+        </>
+    );
+}
+
+export default Overview;

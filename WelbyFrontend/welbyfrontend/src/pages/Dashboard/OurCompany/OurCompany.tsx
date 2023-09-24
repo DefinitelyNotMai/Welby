425f30ae79c21817b71729306cd8866b9350f34e
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-25 03:47:11 +0800

    feat: Render pages and some code refactoring.

--- WelbyFrontend/welbyfrontend/src/pages/Dashboard/OurCompany/OurCompany.tsx
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/OurCompany/OurCompany.tsx
@@ -1,16 +1,55 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, } from '@chakra-ui/react';
+import { useState } from 'react';
 import DashboardTab from '../../../components/Dashboard/Tab';
 import DashboardTabItem from '../../../components/Dashboard/TabItem';
+import CompanyGoals from './CompanyGoals';
+import CoreValues from './CoreValues';
+import MissionAndVision from './MissionAndVision';
+
+type ContentMapping = {
+    [key: string]: React.ReactNode;
+};
 
 const OurCompany = () => {
+    const [selectedItem, setSelectedItem] = useState<string | null>('MissionAndVision');
+
+    const handleItemClick = (itemName: string) => {
+        setSelectedItem(itemName);
+    };
+
+    const contentMapping: ContentMapping = {
+        MissionAndVision: <MissionAndVision />,
+        CompanyGoals: <CompanyGoals />,
+        CoreValues: <CoreValues />,
+    };
+
     return (
-        <Flex flexDirection="row">
+        <Box mt="4" ml="4" minH="full">
             <DashboardTab>
-                <DashboardTabItem>Mission & Vision</DashboardTabItem>
-                <DashboardTabItem>Company Goals</DashboardTabItem>
-                <DashboardTabItem>Core Values</DashboardTabItem>
+                <DashboardTabItem
+                    color={selectedItem === 'MissionAndVision' ? '#89b4fa' : '#bcbcbc'}
+                    onClick={() => handleItemClick('MissionAndVision')}
+                >
+                    Mission & Vision
+                </DashboardTabItem>
+                <DashboardTabItem
+                    color={selectedItem === 'CompanyGoals' ? '#89b4fa' : '#bcbcbc'}
+                    onClick={() => handleItemClick('CompanyGoals')}
+                >
+                    Company Goals
+                </DashboardTabItem>
+                <DashboardTabItem
+                    color={selectedItem === 'CoreValues' ? '#89b4fa' : '#bcbcbc'}
+                    onClick={() => handleItemClick('CoreValues')}
+                >
+                    Core Values
+                </DashboardTabItem>
             </DashboardTab>
-        </Flex>
+
+            <Flex flexDirection="column" h="100vh" overflow="hidden" flex="1">
+                {selectedItem && contentMapping[selectedItem]}
+            </Flex>
+        </Box>
     );
 };
 

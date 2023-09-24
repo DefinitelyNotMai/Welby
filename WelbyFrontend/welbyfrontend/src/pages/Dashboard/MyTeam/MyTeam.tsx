0bd5741dc648ad6fe826cb44f642ecbf423ca27d
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-25 03:44:58 +0800

    feat: Render pages and some code refactoring.

--- WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/MyTeam.tsx
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/MyTeam.tsx
@@ -1,8 +1,13 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, Icon } from '@chakra-ui/react';
 import { useState } from 'react';
+import Section from '../../../components/Dashboard/Section';
 import DashboardTab from '../../../components/Dashboard/Tab';
 import DashboardTabItem from '../../../components/Dashboard/TabItem';
 import Overview from './Overview';
+import Label from '../../../components/Form/Label';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import AddEmployee from '../../../components/Form/AddEmployee';
+import Profile from './Profile';
 
 type ContentMapping = {
     [key: string]: React.ReactNode;
@@ -10,6 +15,11 @@ type ContentMapping = {
 
 const MyTeam = () => {
     const [selectedItem, setSelectedItem] = useState<string | null>('Overview');
+    const [isAddEmployeeOpen, setIsAddEmployeeOpen] = useState(false);
+
+    const toggleAddEmployee = () => {
+        setIsAddEmployeeOpen(!isAddEmployeeOpen);
+    };
 
     const handleItemClick = (itemName: string) => {
         setSelectedItem(itemName);
@@ -17,29 +27,43 @@ const MyTeam = () => {
 
     const contentMapping: ContentMapping = {
         Overview: <Overview />,
-        //TeamProfile: <TeamProfile />,
+        Profile: <Profile />,
     };
 
     return (
-        <Flex flexDirection="column">
-            <DashboardTab>
-                <DashboardTabItem
-                    color={selectedItem === 'Overview' ? '#89b4fa' : '#bcbcbc'}
-                    onClick={() => handleItemClick('Overview')}
-                >
-                    Overview
-                </DashboardTabItem>
-                <DashboardTabItem
-                    color={selectedItem === 'TeamProfile' ? '#89b4fa' : '#bcbcbc'}
-                    onClick={() => handleItemClick('TeamProfile')}
-                >
-                    Team Profile
-                </DashboardTabItem>
-            </DashboardTab>
-            <Flex flexDirection="column" mt="4" ml="4">
-                {selectedItem && contentMapping[selectedItem]}
+        <Grid templateColumns="1fr 2fr" mt="4" ml="4">
+            <Section
+                title="Team"
+                headerComponents={[<Label name="Add Employee" color="#000000" />, <Icon as={AiOutlinePlusCircle} boxSize="8" color="#24a2f0" cursor="pointer" onClick={toggleAddEmployee} />]}
+                borderRadius="1rem 1rem 0 0"
+            >
+            </Section>
+            <Flex flexDirection="column" ml="4">
+                <DashboardTab>
+                    <DashboardTabItem
+                        color={selectedItem === 'Overview' ? '#89b4fa' : '#bcbcbc'}
+                        onClick={() => handleItemClick('Overview')}
+                    >
+                        Overview
+                    </DashboardTabItem>
+                    <DashboardTabItem
+                        color={selectedItem === 'Profile' ? '#89b4fa' : '#bcbcbc'}
+                        onClick={() => handleItemClick('Profile')}
+                    >
+                        Profile
+                    </DashboardTabItem>
+                </DashboardTab>
+                <Box overflow="hidden" flex="1">
+                    {selectedItem && contentMapping[selectedItem]}
+                </Box>
             </Flex>
-        </Flex>
+            {isAddEmployeeOpen && (
+                <AddEmployee
+                    isOpen={isAddEmployeeOpen}
+                    onClose={toggleAddEmployee}
+                />
+            )}
+        </Grid>
     );
 };
 

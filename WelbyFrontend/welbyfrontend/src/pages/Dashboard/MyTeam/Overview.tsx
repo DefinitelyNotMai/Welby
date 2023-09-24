2922d97c11923107279f107ecd155b431cba6e2c
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-09-25 03:45:25 +0800

    feat: Display Charts in MyTeam/Overview.

--- WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/Overview.tsx
+++ WelbyFrontend/welbyfrontend/src/pages/Dashboard/MyTeam/Overview.tsx
@@ -1,11 +1,63 @@
+import { Flex, Grid } from "@chakra-ui/react";
 import Section from "../../../components/Dashboard/Section";
+import Card from '../../../components/Dashboard/Card';
+import { BsPerson } from "react-icons/bs";
+import { TbTargetArrow } from "react-icons/tb";
+import { FaRegThumbsDown, FaRegThumbsUp } from "react-icons/fa";
+import ChartDoughnut from "../../../components/Dashboard/ChartDoughnut";
+import { GiHummingbird, GiMeshNetwork } from "react-icons/gi";
+import { CiDumbbell } from "react-icons/ci";
 
 const Overview = () => {
     return (
-        <>
-            <Section title="Team">
+        <Grid templateRows="1fr 1ft" gap="4" my="4">
+            <Section title="Overall Well-being at Work">
+                {/*<Card icon={BsPerson} dataValue={80} title="Autonomy" />*/}
+                {/*<Card icon={TbTargetArrow} dataValue={70} title="Focus At Work" />*/}
+                {/*<Card icon={FaRegThumbsUp} dataValue={55} title="Positive Emotion" />*/}
+                {/*<Card icon={FaRegThumbsDown} dataValue={25} title="Negative Emotion" />*/}
+                <Grid templateColumns="1fr 1fr" gap="4">
+                    <Card
+                        icon={BsPerson}
+                        dataValue={80}
+                        title="Autonomy" />
+                    <Card
+                        icon={TbTargetArrow}
+                        dataValue={70}
+                        title="Focus At Work"
+                    />
+                    <Card
+                        icon={FaRegThumbsUp}
+                        dataValue={55}
+                        title="Positive Emotion"
+                    />
+                    <Card
+                        icon={FaRegThumbsDown}
+                        dataValue={25}
+                        title="Negative Emotion"
+                    />
+                </Grid>
             </Section>
-        </>
+            <Section title="Overall Pillars to Thrive at Work">
+                <Grid templateColumns="1fr 1fr" gap="4">
+                    <ChartDoughnut
+                        dataValue={75}
+                        icon={GiHummingbird}
+                        title="Autonomy"
+                    />
+                    <ChartDoughnut
+                        dataValue={45}
+                        icon={CiDumbbell}
+                        title="Competence"
+                    />
+                    <ChartDoughnut
+                        dataValue={50}
+                        icon={GiMeshNetwork}
+                        title="Connection"
+                    />
+                </Grid>
+            </Section>
+        </Grid>
     );
 }
 

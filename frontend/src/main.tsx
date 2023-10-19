ec18031322a83e89f3d3e9d310b03efcd0f5f1db
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-10-20 00:32:43 +0800

    feat: enhanced eslint configuration for type-aware linting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/main.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+  );
+}

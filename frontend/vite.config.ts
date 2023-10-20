74ca6387d1dae69a8f92420b4415631ae165f38d
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-10-20 18:10:57 +0800

    feat: installed vite-plugin-node-stdlib-browser

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
 })

ac548e1e8180d0ac41dacf6e30c34c9b39e3d6b5
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-10-20 18:12:17 +0800

    feat: added ChakraUI them config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import theme from "./configuration/theme.ts";
 import "./styles/main.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </React.StrictMode>,

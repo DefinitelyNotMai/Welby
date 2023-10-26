9fefaa60ed01a3a22c9157d7dfc0793c3ae3b094
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-10-27 02:46:20 +0800

    refactor: disabled useRedirect for now

--- frontend/src/hooks/useRedirect.ts
+++ frontend/src/hooks/useRedirect.ts
@@ -1,18 +1,13 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import useUserContext from "./useUserContext";
-
 const useRedirect = () => {
-  const { isLoggedIn } = useUserContext();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate("/dashboard");
-    } else {
-      navigate("/");
-    }
-  }, [isLoggedIn, navigate]);
+  //const { isLoggedIn } = useUserContext();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     navigate("/dashboard");
+  //   } else {
+  //     navigate("/");
+  //   }
+  // }, [isLoggedIn, navigate]);
 };
 
 export default useRedirect;

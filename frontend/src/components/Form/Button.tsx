7792ca2442be7272bc5531f99b5501a5f7822e4f
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-11-07 17:45:17 +0800

    feat: added buttonVariants prop

--- frontend/src/components/Form/Button.tsx
+++ frontend/src/components/Form/Button.tsx
@@ -1,25 +1,49 @@
 import {
   Button as ChakraButton,
   ButtonProps as ChakraButtonProps,
+  Icon,
 } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { ElementType } from "react";
 
-interface ButtonProps extends ChakraButtonProps {
-  children: ReactNode;
-  variant: "primary" | "secondary";
-}
+type ButtonProps = ChakraButtonProps & {
+  children: ChakraButtonProps["children"];
+  buttonVariant: "primary" | "masterCrud" | "sidebar" | "submit";
+  icon?: ElementType;
+};
 
-const Button = ({ children, variant, ...props }: ButtonProps) => {
+const Button = ({
+  children,
+  buttonVariant,
+  icon: IconComponent,
+  ...props
+}: ButtonProps) => {
   const buttonVariants = {
     primary: {
-      backgroundColor: "primary.1",
-      border: "2px solid white",
-      color: "white",
+      backgroundColor: "#24a2f0",
+      borderColor: "#ffffff",
+      borderStyle: "solid",
+      borderWidth: "2px",
+      color: "#ffffff",
     },
-    secondary: {
-      backgroundColor: "primary",
-      border: "2px solid primary.1",
-      color: "black",
+    submit: {
+      backgroundColor: "#ffffff",
+      color: "#6c6c6c",
+    },
+    sidebar: {
+      _hover: { boxShadow: "outline" },
+      backgroundColor: "#ffffff",
+      borderRadius: "none",
+      borderLeftWidth: "5px",
+      boxShadow: "none",
+      fontWeight: "normal",
+      paddingY: 2,
+      marginY: 2,
+      justifyContent: "flex-start",
+    },
+    masterCrud: {
+      backgroundColor: "#ffffff",
+      borderColor: "#ebebeb",
+      borderWidth: "2px",
     },
   };
 
@@ -28,12 +52,13 @@ const Button = ({ children, variant, ...props }: ButtonProps) => {
       _hover={{ boxShadow: "dark-lg" }}
       boxShadow="2xl"
       fontFamily="Montserrat"
-      fontSize={["sm", "md"]}
+      fontSize={["xs", "sm"]}
       fontWeight="bold"
       width="100%"
-      {...buttonVariants[variant]}
+      {...buttonVariants[buttonVariant]}
       {...props}
     >
+      {IconComponent && <Icon as={IconComponent} marginX="3" />}
       {children}
     </ChakraButton>
   );

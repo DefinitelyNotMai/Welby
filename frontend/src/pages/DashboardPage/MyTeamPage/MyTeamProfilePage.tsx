1116aaa8b007bd7aa8d7b0f8a50b0626ae9e698d
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-12-01 20:24:25 +0800

    feat: conditional rendering of member profile based on FirstLogIn

--- frontend/src/pages/DashboardPage/MyTeamPage/MyTeamProfilePage.tsx
+++ frontend/src/pages/DashboardPage/MyTeamPage/MyTeamProfilePage.tsx
@@ -2,7 +2,16 @@ import { useOutletContext } from "react-router-dom";
 import { Section } from "../../../components/DataDisplay/Section";
 import { useEffect, useState } from "react";
 import { fetchData } from "../../../api/fetchData";
-import { Avatar, Button, Divider, Flex, Grid, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Flex,
+  Grid,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import { RiErrorWarningLine } from "react-icons/ri";
 import { EMPLOYEE_DATA, Employee } from "../../../data/employee";
 
 type TeamMember = {
@@ -17,7 +26,7 @@ export const MyTeamProfilePage = () => {
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const employeeUrl = "https://localhost:44373/api/GetEmployee";
+      const employeeUrl = "https://localhost:44373/api/GetEmployees";
 
       try {
         const result = await fetchData(employeeUrl, {
@@ -31,105 +40,129 @@ export const MyTeamProfilePage = () => {
       }
     };
     fetchUserData();
-  }, [selectedEmployee.EmployeeId]);
+  }, [selectedEmployee.EmployeeId, employeeData]);
 
-  return (
-    <Section
-      borderRadius="1rem 0 0 0"
-      marginTop={4}
-      title={`${employeeData.First_Name} ${employeeData.Middle_Name} ${employeeData.Last_Name}`}
-      width="full"
-    >
-      <Flex flexDirection="column" gap={4} height="full">
-        <Avatar
-          alignSelf="center"
-          boxShadow="xl"
-          boxSize={48}
-          src={employeeData.ProfilePhoto}
-        />
-        <Text
-          alignSelf="center"
-          color="black"
-          fontSize="1.125rem"
-          fontWeight="semibold"
-        >
-          {employeeData.Nickname}
-        </Text>
-        <Divider />
-        <Flex
-          alignSelf="center"
-          flexDirection="column"
-          fontWeight="medium"
-          gap={4}
-          width="50%"
-        >
-          <Text color="#bcbcbc">Personal Information</Text>
-          <Grid gap={8} templateColumns="1fr 1fr" width="full">
-            <Flex alignItems="flex-end" flexDirection="column" gap={2}>
-              <Text color="#bcbcbc">First Name:</Text>
-              <Text color="#bcbcbc">Middle Name:</Text>
-              <Text color="#bcbcbc">Last Name:</Text>
-              <Text color="#bcbcbc">Birthday:</Text>
-              <Text color="#bcbcbc">Address:</Text>
-              <Text color="#bcbcbc">Location:</Text>
-            </Flex>
-            <Flex alignItems="flex-start" flexDirection="column" gap={2}>
-              <Text color="#34313a">{employeeData.First_Name}</Text>
-              <Text color="#34313a">{employeeData.Middle_Name}</Text>
-              <Text color="#34313a">{employeeData.Last_Name}</Text>
-              <Text color="#34313a">{employeeData.Birthday}</Text>
-              <Text color="#34313a">{employeeData.Address}</Text>
-            </Flex>
-          </Grid>
+  if (employeeData.FirstLogIn === true) {
+    return (
+      <Section
+        borderRadius="1rem 0 0 0"
+        marginTop={4}
+        title={`${employeeData.First_Name} ${employeeData.Middle_Name} ${employeeData.Last_Name}`}
+        width="full"
+      >
+        <Flex flexDirection="column" gap={4} height="full">
+          <Avatar
+            alignSelf="center"
+            boxShadow="xl"
+            boxSize={48}
+            src={employeeData.ProfilePhoto}
+          />
+          <Text
+            alignSelf="center"
+            color="black"
+            fontSize="1.125rem"
+            fontWeight="semibold"
+          >
+            {employeeData.Nickname}
+          </Text>
+          <Divider />
+          <Flex
+            alignSelf="center"
+            flexDirection="column"
+            fontWeight="medium"
+            gap={4}
+            width="50%"
+          >
+            <Text color="#bcbcbc">Personal Information</Text>
+            <Grid gap={8} templateColumns="1fr 1fr" width="full">
+              <Flex alignItems="flex-end" flexDirection="column" gap={2}>
+                <Text color="#bcbcbc">First Name:</Text>
+                <Text color="#bcbcbc">Middle Name:</Text>
+                <Text color="#bcbcbc">Last Name:</Text>
+                <Text color="#bcbcbc">Birthday:</Text>
+                <Text color="#bcbcbc">Location:</Text>
+              </Flex>
+              <Flex alignItems="flex-start" flexDirection="column" gap={2}>
+                <Text color="#34313a">{employeeData.First_Name}</Text>
+                <Text color="#34313a">{employeeData.Middle_Name}</Text>
+                <Text color="#34313a">{employeeData.Last_Name}</Text>
+                <Text color="#34313a">{employeeData.Birthday}</Text>
+                <Text color="#34313a">
+                  {employeeData.Address}, {employeeData.CountryDisplay}
+                </Text>
+              </Flex>
+            </Grid>
+          </Flex>
+          <Divider />
+          <Flex
+            alignSelf="center"
+            flexDirection="column"
+            fontWeight="medium"
+            gap={4}
+            width="50%"
+          >
+            <Text color="#bcbcbc">Contact Information</Text>
+            <Grid gap={8} templateColumns="1fr 1fr" width="full">
+              <Flex alignItems="flex-end" flexDirection="column" gap={2}>
+                <Text color="#bcbcbc">Phone Number:</Text>
+                <Text color="#bcbcbc">Email:</Text>
+              </Flex>
+              <Flex alignItems="flex-start" flexDirection="column" gap={2}>
+                <Text color="#34313a">{employeeData.Phone_Number}</Text>
+                <Text color="#34313a">{employeeData.Email}</Text>
+              </Flex>
+            </Grid>
+          </Flex>
+          <Divider />
+          <Flex
+            alignSelf="center"
+            flexDirection="column"
+            fontWeight="medium"
+            gap={4}
+            width="50%"
+          >
+            <Text color="#bcbcbc">Socials</Text>
+            <Grid gap={8} templateColumns="1fr 1fr" width="full">
+              <Flex alignItems="flex-end" flexDirection="column" gap={2}>
+                <Text color="#bcbcbc">LinkedIn:</Text>
+                <Text color="#bcbcbc">Facebook:</Text>
+                <Text color="#bcbcbc">TikTok:</Text>
+                <Text color="#bcbcbc">Instagram:</Text>
+              </Flex>
+              <Flex alignItems="flex-start" flexDirection="column" gap={2}>
+                <Text color="#34313a">{employeeData.Linkedin}</Text>
+                <Text color="#34313a">{employeeData.Facebook}</Text>
+                <Text color="#34313a">{employeeData.TikTok}</Text>
+                <Text color="#34313a">{employeeData.Instagram}</Text>
+              </Flex>
+            </Grid>
+          </Flex>
+          <Button alignSelf="center" marginTop={8} width="25%">
+            See Full Profile
+          </Button>
         </Flex>
-        <Divider />
+      </Section>
+    );
+  } else {
+    return (
+      <Section
+        borderRadius="1rem 0 0 0"
+        marginTop={4}
+        title="Employee Profile"
+        width="full"
+      >
         <Flex
-          alignSelf="center"
+          alignItems="center"
           flexDirection="column"
-          fontWeight="medium"
-          gap={4}
-          width="50%"
+          height="full"
+          justifyContent="center"
         >
-          <Text color="#bcbcbc">Contact Information</Text>
-          <Grid gap={8} templateColumns="1fr 1fr" width="full">
-            <Flex alignItems="flex-end" flexDirection="column" gap={2}>
-              <Text color="#bcbcbc">Phone Number:</Text>
-              <Text color="#bcbcbc">Email:</Text>
-            </Flex>
-            <Flex alignItems="flex-start" flexDirection="column" gap={2}>
-              <Text color="#34313a">{employeeData.Phone_Number}</Text>
-              <Text color="#34313a">{employeeData.Email}</Text>
-            </Flex>
-          </Grid>
+          <Icon as={RiErrorWarningLine} boxSize="64" color="#24a2f0" />
+          <Text color="#34313a" fontSize="1.25rem" fontWeight={700}>
+            Employee has not logged in yet
+          </Text>
         </Flex>
-        <Divider />
-        <Flex
-          alignSelf="center"
-          flexDirection="column"
-          fontWeight="medium"
-          gap={4}
-          width="50%"
-        >
-          <Text color="#bcbcbc">Socials</Text>
-          <Grid gap={8} templateColumns="1fr 1fr" width="full">
-            <Flex alignItems="flex-end" flexDirection="column" gap={2}>
-              <Text color="#bcbcbc">LinkedIn:</Text>
-              <Text color="#bcbcbc">Facebook:</Text>
-              <Text color="#bcbcbc">TikTok:</Text>
-              <Text color="#bcbcbc">Instagram:</Text>
-            </Flex>
-            <Flex alignItems="flex-start" flexDirection="column" gap={2}>
-              <Text color="#34313a">{employeeData.Linkedin}</Text>
-              <Text color="#34313a">{employeeData.Facebook}</Text>
-              <Text color="#34313a">{employeeData.TikTok ?? "null"}</Text>
-              <Text color="#34313a">{employeeData.Instagram}</Text>
-            </Flex>
-          </Grid>
-        </Flex>
-        <Button alignSelf="center" marginTop={8} width="25%">
-          See Full Profile
-        </Button>
-      </Flex>
-    </Section>
-  );
+      </Section>
+    );
+  }
 };

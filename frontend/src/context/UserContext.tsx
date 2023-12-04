5de57cb9f980daf7420ad462000e9219e623b978
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-12-04 21:14:29 +0800

    feat: change user role context to string

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -5,11 +5,11 @@ type UserContextType = {
   companyId: number;
   email: string;
   phone: string;
-  role: number;
+  role: string;
   setCompanyId: (id: number) => void;
   setEmail: (email: string) => void;
   setPhone: (phone: string) => void;
-  setRole: (role: number) => void;
+  setRole: (role: string) => void;
 };
 
 type UserContextProviderProps = {
@@ -22,7 +22,7 @@ export const UserContextProvider = ({ children }: UserContextProviderProps) => {
   const [companyId, setCompanyId] = useState<number>(0);
   const [email, setEmail] = useState<string>("");
   const [phone, setPhone] = useState<string>("");
-  const [role, setRole] = useState<number>(0);
+  const [role, setRole] = useState<string>("");
 
   return (
     <UserContext.Provider

c4155bbbf4665b46024d3b36eca8679569ad006d
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-12-07 06:14:26 +0800

    unfinished: Company modal

--- frontend/src/components/Modal/AdminView/CompanyModal.tsx
+++ frontend/src/components/Modal/AdminView/CompanyModal.tsx
@@ -1,41 +1,4 @@
-import { Button, Flex, Grid, Input, Text, Textarea } from "@chakra-ui/react";
-import { AdminViewModal } from "./AdminViewModal";
-import { SelectCountry, SelectIndustryType } from "../../Form/Select";
-import { FormItem } from "../../Form/FormItem";
-import { UploadPhoto } from "../../Form/UploadPhoto";
-
-type CompanyData = {
-  CompanyId: number;
-  Logo: string;
-  Name: string;
-  Email: string;
-  Phone_Number: string;
-  Website: string;
-  Vision: string;
-  Mission: string;
-  CountryId: number;
-  IndustryTypeId: number;
-  FoundingDate: string;
-};
-
-type CompanyAddUpdateProps = CompanyData & {
-  handleAddUpdate: () => void;
-  handleCancel: () => void;
-  isOpen: boolean;
-  onClose: () => void;
-  updateFields: (fields: Partial<CompanyData>) => void;
-};
-
-const CompanyAdd = ({ isOpen, onClose }: CompanyAddUpdateProps) => {
-  //<UploadPhoto id="company-logo" name="company-logo" label="Company Logo" />
-  //<FormItem isRequired></FormItem>
-  return (
-    <AdminViewModal header="Add Company" isOpen={isOpen} onClose={onClose}>
-      CompanyAdd
-    </AdminViewModal>
-  );
-};
-
+/*
 const CompanyUpdate = ({
   Logo,
   Name,
@@ -174,38 +137,301 @@ const CompanyUpdate = ({
     </AdminViewModal>
   );
 };
+*/
+// lib
+import axios from "axios";
+import {
+  Button,
+  Flex,
+  Input,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { Form } from "react-router-dom";
 
-type CompanyDeleteProps = CompanyData & {
-  handleCancel: () => void;
-  handleDelete: () => void;
+// local
+import { FormEvent } from "react";
+import { FormItem } from "../../Form/FormItem";
+import { Company } from "../../../data/company";
+
+type CompanyModalProps = Company & {
   isOpen: boolean;
   onClose: () => void;
+  updateFields?: (fields: Partial<Company>) => void;
 };
 
-const CompanyDelete = ({
-  handleCancel,
-  handleDelete,
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export const CompanyAdd = ({
   isOpen,
   onClose,
-  Name,
-}: CompanyDeleteProps) => {
+  updateFields,
+  ...companyData
+}: CompanyModalProps) => {
+  const toast = useToast();
+
+  const handleAdd = (e: FormEvent) => {
+    e.preventDefault();
+    const addCompanyUrl = "https://localhost:44373/api/AddCompany";
+    const company = {
+      Company: companyData.Company,
+      Biological: true,
+      Encoded_By: 24287,
+    };
+
+    axios
+      .post(addCompanyUrl, company, config)
+      .then(() => {
+        toast({
+          description: `Company "${companyData.Company}" has been added.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "success",
+          title: "SUCCESS",
+        });
+        onClose();
+      })
+      .catch((error) => {
+        console.error("An error occurred: ", error);
+        toast({
+          description: `Failed to add Company "${companyData.Company}". Please try again.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "error",
+          title: "Error",
+        });
+      });
+  };
+
   return (
-    <AdminViewModal header="Delete Company" isOpen={isOpen} onClose={onClose}>
-      <Flex flexDirection="column" gap={8}>
-        <Text color="white" textAlign="center">
-          Are you sure you want to delete {Name}?
-        </Text>
-        <Flex flexDirection="row" gap={8}>
-          <Button variant="submit" onClick={handleCancel}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleDelete}>
-            Delete
-          </Button>
-        </Flex>
-      </Flex>
-    </AdminViewModal>
+    <Modal
+      closeOnEsc={false}
+      closeOnOverlayClick={false}
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <ModalContent maxHeight="90vh" minWidth="50%" overflowY="auto">
+        <ModalHeader>Add Company</ModalHeader>
+        <ModalCloseButton />
+        <Form onSubmit={handleAdd}>
+          <Flex
+            alignItems="center"
+            flexDirection="column"
+            gap={4}
+            justifyContent="center"
+            padding={8}
+          >
+            <FormItem htmlFor="company" isRequired label="Title">
+              <Input
+                id="company"
+                name="company"
+                onChange={(e) => updateFields?.({ Company: e.target.value })}
+                placeholder="Title"
+                type="text"
+                value={companyData.Company}
+              />
+            </FormItem>
+            <Flex flexDirection="row" gap={4}>
+              <Button onClick={onClose} variant="submit">
+                Cancel
+              </Button>
+              <Button type="submit" variant="primary">
+                Add
+              </Button>
+            </Flex>
+          </Flex>
+        </Form>
+      </ModalContent>
+    </Modal>
   );
 };
 
-export { CompanyAdd, CompanyUpdate, CompanyDelete };
+export const CompanyUpdate = ({
+  isOpen,
+  onClose,
+  updateFields,
+  ...companyData
+}: CompanyModalProps) => {
+  const toast = useToast();
+
+  const handleUpdate = (e: FormEvent) => {
+    e.preventDefault();
+    const updateCompanyUrl = "https://localhost:44373/api/UpdateCompany";
+    const company = {
+      CompanyId: companyData.CompanyId,
+      Logo: companyData.Logo,
+      Name: companyData.Name,
+      Email: companyData.Email,
+      Phone_Number: companyData.Phone_Number,
+      Website: companyData.Website,
+      FoundingDate: companyData.FoundingDate,
+      Address: companyData.Address,
+      CountryId: companyData.CountryId,
+      IndustryTypeId: companyData.IndustryTypeId,
+      Active: true,
+      Encoded_By: 24287,
+    };
+
+    axios
+      .patch(updateCompanyUrl, company, config)
+      .then(() => {
+        toast({
+          description: `Company "${companyData.Company}" has been updated.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "success",
+          title: "SUCCESS",
+        });
+        onClose();
+      })
+      .catch((error) => {
+        console.error("An error occurred: ", error);
+        toast({
+          description: `Failed to update company "${companyData.Company}". Please try again.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "error",
+          title: "Error",
+        });
+      });
+  };
+
+  return (
+    <Modal
+      closeOnEsc={false}
+      closeOnOverlayClick={false}
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <ModalContent maxHeight="90vh" minWidth="50%" overflowY="auto">
+        <ModalHeader>Update Company</ModalHeader>
+        <ModalCloseButton />
+        <Form onSubmit={handleUpdate}>
+          <Flex
+            alignItems="center"
+            flexDirection="column"
+            gap={4}
+            justifyContent="center"
+            padding={8}
+          >
+            <FormItem htmlFor="company" isRequired label="Title">
+              <Input
+                id="company"
+                name="company"
+                onChange={(e) => updateFields?.({ Company: e.target.value })}
+                placeholder="Title"
+                type="text"
+                value={companyData.Company}
+              />
+            </FormItem>
+            <Flex flexDirection="row" gap={4}>
+              <Button onClick={onClose} variant="submit">
+                Cancel
+              </Button>
+              <Button type="submit" variant="primary">
+                Update
+              </Button>
+            </Flex>
+          </Flex>
+        </Form>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export const CompanyDelete = ({
+  isOpen,
+  onClose,
+  ...companyData
+}: CompanyModalProps) => {
+  const toast = useToast();
+
+  const handleDelete = (e: FormEvent) => {
+    e.preventDefault();
+    const deleteCompanyUrl = "https://localhost:44373/api/RemoveCompany";
+    const company = {
+      CompanyId: companyData.CompanyId,
+      Encoded_By: 24287,
+    };
+
+    axios
+      .patch(deleteCompanyUrl, company, config)
+      .then(() => {
+        toast({
+          description: `Company "${companyData.Name}" has been deleted.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "success",
+          title: "SUCCESS",
+        });
+        onClose();
+      })
+      .catch((error) => {
+        console.error("An error occurred: ", error);
+        toast({
+          description: `Failed to delete Company "${companyData.Name}". Please try again.`,
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+          status: "error",
+          title: "Error",
+        });
+      });
+  };
+
+  return (
+    <Modal
+      closeOnEsc={false}
+      closeOnOverlayClick={false}
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <ModalContent minWidth="35%">
+        <ModalHeader>Delete Company</ModalHeader>
+        <ModalCloseButton />
+        <Form onSubmit={handleDelete}>
+          <Flex
+            alignItems="center"
+            flexDirection="column"
+            gap={8}
+            justifyContent="center"
+          >
+            <Text color="#ffffff" fontSize="1.25rem">
+              Are you sure you want to delete &quot;
+              {companyData.Name}
+              &quot;?
+            </Text>
+            <Flex flexDirection="row" gap={4} marginBottom={4}>
+              <Button onClick={onClose} variant="submit">
+                Cancel
+              </Button>
+              <Button type="submit" variant="primary">
+                Delete
+              </Button>
+            </Flex>
+          </Flex>
+        </Form>
+      </ModalContent>
+    </Modal>
+  );
+};

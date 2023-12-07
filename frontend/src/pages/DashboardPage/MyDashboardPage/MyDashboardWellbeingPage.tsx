052feb5c61fcde3f3d5743f6a4f96dc2220ea78c
DefinitelyNotMai <neilfrancispalmares@tutanota.com>
2023-12-07 22:15:46 +0800

    feat: conditional rendering of Section and added button for downloading scores as excel spreadsheet in MyDashboardWellbeingPage

--- frontend/src/pages/DashboardPage/MyDashboardPage/MyDashboardWellbeingPage.tsx
+++ frontend/src/pages/DashboardPage/MyDashboardPage/MyDashboardWellbeingPage.tsx
@@ -3,8 +3,6 @@ import { useContext, useState } from "react";
 import { Section } from "../../../components/DataDisplay/Section";
 import { ChartBar } from "../../../components/Charts/ChartBar";
 import { PredictionChart } from "../../../components/Charts/PredictionChart";
-import { DailyCheckin } from "../../../components/Modal/DailyCheckin";
-import { QuarterlyAssessment } from "../../../components/Modal/QuarterlyAssessment";
 import { UserContext } from "../../../context/UserContext";
 
 export const MyDashboardWellbeingPage = () => {
@@ -12,6 +10,10 @@ export const MyDashboardWellbeingPage = () => {
 
   const userContext = useContext(UserContext);
   const [modal, setModal] = useState<string>("");
+
+  const handleDownload = () => {
+    alert("HELLO");
+  };
 
   return (
     <Grid templateRows="1fr 1fr" gap={4} width="full" marginBottom={4}>
@@ -31,37 +33,26 @@ export const MyDashboardWellbeingPage = () => {
           <ChartBar />
         </Flex>
       </Section>
-      <Section
-        title="The Pillars of your Ability to Thrive"
-        headerComponents={[
-          <Button
-            key={1}
-            marginRight={16}
-            onClick={() => setModal("quarterly-assessment")}
-          >
-            Take Quarterly Assessment
-          </Button>,
-        ]}
-      >
-        {userContext.role === "Company Admin" ||
-        userContext.role === "Leader" ? (
-          <></>
-        ) : (
+      {userContext.role === "Company Admin" || userContext.role === "Leader" ? (
+        <Section
+          title="The Pillars of your Ability to Thrive"
+          headerComponents={[
+            <Button key={1} marginRight={16} onClick={handleDownload}>
+              Download Scores as Excel Spreadsheet
+            </Button>,
+          ]}
+        >
           <PredictionChart />
-        )}
-      </Section>
-      {modal === "daily-checkin" && (
+        </Section>
+      ) : (
+        <></>
+      )}
+      {/*modal === "daily-checkin" && (
         <DailyCheckin
           isOpen={modal === "daily-checkin"}
           onClose={() => setModal("")}
         />
-      )}
-      {modal === "quarterly-assessment" && (
-        <QuarterlyAssessment
-          isOpen={modal === "quarterly-assessment"}
-          onClose={() => setModal("")}
-        />
-      )}
+      )*/}
     </Grid>
   );
 };
